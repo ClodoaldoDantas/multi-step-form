59f5c3a278afb6edc9aeba2f738cf3adeea5b48a
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2023-05-20 20:20:39 -0300

    feat: global styles

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,26 @@
+import { Poppins, Merriweather } from 'next/font/google'
+import { ReactNode } from 'react'
+import styles from './styles.module.scss'
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '500', '600'],
+})
+
+const merriweather = Merriweather({
+  subsets: ['latin'],
+  weight: ['900'],
+  variable: '--font-heading',
+})
+
+interface LayoutProps {
+  children: ReactNode
+}
+
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <main className={`${poppins.className} ${merriweather.variable}`}>
+      <div className={styles.container}>{children}</div>
+    </main>
+  )
+}
