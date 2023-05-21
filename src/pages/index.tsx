3c02a58e61b3ef3e8cfaaf76529c5cec74f3718f
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2023-05-21 14:19:02 -0300

    feat: homepage form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,8 @@ import { Button } from '@/components/Button'
 import { Card } from '@/components/Card'
 import { MultiStep } from '@/components/MultiStep'
 
+import styles from '@/styles/Home.module.scss'
+
 export default function Home() {
   return (
     <Card>
@@ -14,6 +16,64 @@ export default function Home() {
         <p className="step-description">
           Please provide your personal details so we can get to know you better.
         </p>
+
+        <div className={styles.formGrid}>
+          <div className={styles.formGroup}>
+            <label className={styles.formLabel} htmlFor="name">
+              Full Name
+            </label>
+
+            <input
+              className={styles.formField}
+              type="text"
+              name="name"
+              id="name"
+              placeholder="John Doe"
+            />
+          </div>
+
+          <div className={styles.formGroup}>
+            <label className={styles.formLabel} htmlFor="email">
+              Email Address
+            </label>
+
+            <input
+              className={styles.formField}
+              type="text"
+              name="email"
+              id="email"
+              placeholder="name@email.com"
+            />
+          </div>
+
+          <div className={styles.formGroup}>
+            <label className={styles.formLabel} htmlFor="phone">
+              Phone Number
+            </label>
+
+            <input
+              className={styles.formField}
+              type="text"
+              name="phone"
+              id="phone"
+              placeholder="+91 1234567890"
+            />
+          </div>
+
+          <div className={styles.formGroup}>
+            <label className={styles.formLabel} htmlFor="github">
+              Portfolio/GitHub Link
+            </label>
+
+            <input
+              className={styles.formField}
+              type="text"
+              name="github"
+              id="github"
+              placeholder="github.com/rishipurwar1"
+            />
+          </div>
+        </div>
       </Card.Body>
 
       <Card.Footer justify="flex-end">
